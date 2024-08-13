c5ffc79c6bc8ee2ea5ee0cb98d0686cf553dd6c4
kyj102 <yain4746@naver.com>
2024-08-13 21:29:58 +0900

    데스크탑, 모바일 완료

--- login.js
+++ login.js
@@ -1,1 +1,25 @@
 import "./header/header.js"
+
+const faEye = document.querySelector(".fa-eye-slash");
+console.log(faEye);
+const pwInput = document.querySelector("#password");
+console.log(pwInput);
+
+faEye.addEventListener("click", () => {
+  const type =
+  pwInput.getAttribute("type") === "password" ? "text" : "password";
+  pwInput.setAttribute("type", type);
+});
+
+// 로그인 유효성 검사
+// 사용자로부터 아이디와 비밀번호의 값을 받는다.
+// 아이디가 공란일 경우, 알림창에 "아이디를 입력하세요!"
+// 비밀번호가 공란일 경우, 알림창에 "비밀번호를 입력하세요!"
+// 아이디와 비밀번호가 둘 다 공란일 경우, 알림창에 "아이디와 비밀번호를 입력하세요!"
+
+const id = document.querySelector("#id");
+const pw = document.querySelector("#password");
+
+const checkPassword = () => {
+  
+};
