bbbc3736cd665670be124bf2620e2d4838248c62
MacUser <macuser@MacUserui-MacBookPro.local>
2024-08-23 17:46:11 +0900

    0823 Update

--- cart/cart02.js
+++ cart/cart02.js
@@ -1,6 +1,19 @@
 // header
 import "./header/header.js";
 
+// JSON 데이터 불러오기
+fetch("./data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    // 데이터를 활용하여 원하는 작업 수행
+    console.log(data);
+
+    // 예를 들어, 데이터로 상품 목록을 생성하거나 총 가격을 계산할 수 있습니다.
+  })
+  .catch((error) => console.error("Error loading JSON data:", error));
+
+
+
 // 홈페이지 이동
 document
   .querySelector(".checkout-button")
