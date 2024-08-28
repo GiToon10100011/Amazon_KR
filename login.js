3a55973de44e412d5497fe7379e2bdeb4ed4f34f
kyj102 <yain4746@naver.com>
2024-08-28 18:02:14 +0900

    240828 Updated

--- login.js
+++ login.js
@@ -1,9 +1,7 @@
 import "./header/header.js"
 
 const faEye = document.querySelector(".fa-eye-slash");
-console.log(faEye);
 const pwInput = document.querySelector("#password");
-console.log(pwInput);
 
 faEye.addEventListener("click", () => {
   const type =
@@ -43,4 +41,47 @@ e.preventDefault();
   } else {
     document.querySelector("#error_pw").innerText = "";
   };
+});
+
+// // 사용자가 입력한 아이디에 한글, 특수기호가 있는지 검사한다.
+// // 있다면 알림창으로 "양식에 맞는 아이디를 올바르게 입력해주세요!"
+// // 없다면 비밀번호 탭으로 넘어가도록 한다.
+// const idInput = document.querySelector("#id").value;
+// const invalidPattern = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣`~!@#$%^&*()_+={}\[\]|\\:;"'<>,.?/]/;
+
+// if(invalidPattern.test(idInput)) {
+//   alert("양식에 맞는 아이디를 올바르게 입력해주세요!");
+//   return false;
+// } else {
+//   document.querySelector(".log_id").classList.add(".log_pw");
+//   document.querySelector("log_pw").classList.remove(".log_pw");
+//   return false;
+// }
+
+// 회원, 비회원 클릭시 인풋박스 변경
+const btns = document.querySelectorAll("h2");
+const forms = document.querySelectorAll(".middle");
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", function() {
+    btns.forEach((sibling) => {
+      if (sibling !== btn) {
+        sibling.classList.remove("active");
+      }
+    });
+    this.classList.add("active");
+
+    const form = document.querySelector(".formMiddle");
+    const contents = form.querySelectorAll(".mem");
+
+    contents.forEach((content) => {
+      content.classList.remove("active");
+    });
+
+    const targetId = btn.getAttribute("data-log");
+    const targetContent = document.getElementById(targetId);
+    if (targetContent) {
+      targetContent.classList.add("active");
+    }
+  });
 });
--- member.js
+++ member.js
@@ -1,1 +1,74 @@
 import "./header/header.js"
+
+// Accordian Event
+const firstContent = document.querySelectorAll(".accordion .content");
+firstContent[0].style.display = "block";
+const titles = document.querySelectorAll(".title");
+titles.forEach((title) => {
+  // 1. 선택된 아이템 1개가 누구인지 알게 하는 함수(모두의 액티브를 제거)
+  title.addEventListener("click", () => {
+    document.querySelectorAll(".content").forEach((item) => {
+      item.style.display = "none";
+    });
+
+    // 2. 미선택 아이템 6개를 구별하기 위한 함수(반복문)
+    titles.forEach((othertitle) => {
+      if(othertitle !== title) {
+        othertitle.classList.remove("active");
+      }
+    });
+
+    // 4. 선택받은 아이템의 콘텐츠를 끌어오는 함수
+    let content = title.nextElementSibling;
+
+    // 3. 선택받은 애가 다시 클릭당하면 액티브 제거, 최초로 클릭받은 아이템에게는 액티브 부여
+    // 5. 선택받은 아이템의 콘텐츠에 opacity값을 1로 부여해서 보여지도록 한다.
+    if(title.classList.contains("active")) {
+      title.classList.remove("active");
+      content.style.display = "none";
+    } else {
+      title.classList.add("active");
+      content.style.display = "block";
+    }
+  });
+});
+
+// 배송정보 입력
+// 사용자로부터 정보값 받기
+  const elZonecode = document.querySelector("#zonecode");
+  const elRoadAddress = document.querySelector("#roadAddress");
+  const elRoadAddressDetail = document.querySelector("#roadAddressDetail");
+  const elResults = document.querySelectorAll(".el_result");
+
+  // 주소검색창 열기
+  const onClickSearch = () => {
+    console.log(1);
+    new daum.Postcode({
+      oncomplete: function (data) {
+        console.log(data);
+        elZonecode.setAttribute("value", data.zonecode);
+        elRoadAddress.setAttribute("value", data.address);
+      },
+    }).open();
+  };
+
+  // 사용자로부터 입력받은 정보값 재확인
+  const register = () => {
+    console.log(`우편번호: ${elZonecode.getAttribute("value")}`);
+    console.log(`주소: ${elRoadAddress.getAttribute("value")}`);
+    console.log(`상세주소: ${elRoadAddressDetail.getAttribute("value")}`);
+    elResults[0].innerHTML = elZonecode.getAttribute("value");
+    elResults[1].innerHTML = elRoadAddress.getAttribute("value");
+    elResults[2].innerHTML = elRoadAddressDetail.getAttribute("value");
+  };
+
+  // 이벤트 추가
+  document.querySelector("#search-btn").addEventListener("click", () => {
+    onClickSearch();
+  });
+  document.querySelector("#register-btn").addEventListener("click", () => {
+    register();
+  });
+  elRoadAddressDetail.addEventListener("change", (e) => {
+    elRoadAddressDetail.setAttribute("value", e.target.value);
+  });
