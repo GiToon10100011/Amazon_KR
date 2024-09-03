c68bf0d916df05708606dc335905416d847e266e
kyj102 <yain4746@naver.com>
2024-09-03 15:07:15 +0900

    merge with Login

--- Login/login.js
+++ Login/login.js
@@ -1,6 +1,6 @@
-import "../header/header.js"
+import "./header/header.js"
 
-const faEye = document.querySelector(".fa-eye-slash");
+const faEye = document.querySelector(".fa-eye");
 const pwInput = document.querySelector("#password");
 
 faEye.addEventListener("click", () => {
@@ -9,15 +9,7 @@ faEye.addEventListener("click", () => {
   pwInput.setAttribute("type", type);
 });
 
-// 로그인 유효성 검사
-// 사용자로부터 아이디와 비밀번호의 값을 받는다.
-// 아이디가 공란일 경우, 알림창에 "아이디를 입력하세요!"
-// 비밀번호가 공란일 경우, 알림창에 "비밀번호를 입력하세요!"
-// 아이디와 비밀번호가 둘 다 공란일 경우, 알림창에 "아이디와 비밀번호를 입력하세요!"
-
-// const id = document.querySelector("#id");
-// const pw = document.querySelector("#password");
-
+// 로그인 유효성검사
 const LogInBtn = document.querySelector("#loginSubmit");
 
 LogInBtn.addEventListener("click", (e) => {
@@ -42,6 +34,47 @@ e.preventDefault();``
     document.querySelector("#error_pw").innerText = "";
   };
 });
+
+
+// 비회원 주문조회 유효성검사
+const nonMemBtn = document.querySelector("#loginSubmit");
+
+nonMemBtn.addEventListener("click", (e) => {
+e.preventDefault();``
+
+  const name = document.querySelector("#name").value;
+  const nonMail = document.querySelector("#nonMail").value;
+
+  let isValid = true;
+
+  if (name === "") {
+    document.querySelector("#error_name").innerText = "주문자명을 올바르게 입력해주세요!";
+    isValid = false;
+  } else {
+    document.querySelector("#error_name").innerText = "";
+  };
+
+  if (nonMail === "") {
+    document.querySelector("#error_mail").innerText = "주문조회에 사용될 이메일을 입력해주세요!";
+    isValid = false;
+  } else {
+    document.querySelector("#error_mail").innerText = "";
+  };
+});
+
+// 비회원 주문조회 이메일형식 검증 정규표현식
+document.getElementById('nonMail').addEventListener('input', function() {
+  const emailInput = this.value;
+  const errorDiv = document.getElementById('error_mail');
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (!emailPattern.test(emailInput) && emailInput !== "") {
+      errorDiv.innerHTML = "이메일 형식이 올바르지 않습니다!";
+  } else {
+      errorDiv.innerHTML = "";
+  }
+});
+
 
 
 // 회원, 비회원 클릭시 인풋박스 변경
--- Login/membership/member.js
+++ Login/membership/member.js
@@ -33,6 +33,44 @@ titles.forEach((title) => {
   });
 });
 
+
+// 카드번호 4자리 단락회로평가
+function autoTab(event) {
+  const input = event.target;
+  const nextInput = input.nextElementSibling;
+
+  // 4자릿수가 모두 입력되면 포커스 자동이동
+  if (input.value.length === 4 && nextInput) {
+      nextInput.focus();
+  }
+
+  // 마지막 카드번호 입력 후 cvv인풋박스 활성화
+  if (input.id === 'cardNum4' && input.value.length === 4) {
+      document.getElementById('cvv').focus();
+  }
+}
+
+document.getElementById('cardNum1').addEventListener('input', autoTab);
+document.getElementById('cardNum2').addEventListener('input', autoTab);
+document.getElementById('cardNum3').addEventListener('input', autoTab);
+document.getElementById('cardNum4').addEventListener('input', autoTab);
+
+
+
+document.getElementById('exDate').addEventListener('input', function() {
+  const input = this.value;
+  const submitBtn = document.getElementById('submitBtn');
+  
+  // 날짜형식이 MM/YY임을 검증하는 정규표현식
+  const datePattern = /^(0[1-9]|1[0-2])\/[0-9]{2}$/;
+  
+  // 사용자 입력값이 형식에 옳으면 카드 등록버튼 자동 활성화
+  if (datePattern.test(input)) {
+      submitBtn.focus();
+  }
+});
+
+
 // 배송정보 입력
 // 사용자로부터 정보값 받기
   const elZonecode = document.querySelector("#zonecode");
@@ -90,3 +128,76 @@ titles.forEach((title) => {
   document.querySelector(".el_btn2").addEventListener("click", () => {
     register2();
   });
+
+
+// address error
+const adrBtn = document.querySelector("#register-btn");
+
+adrBtn.addEventListener("click", (e) => {
+e.preventDefault();
+
+  const zonecode = document.querySelector("#zonecode").value;
+  const roadAddress = document.querySelector("#roadAddress").value;
+  const roadAddressDetail = document.querySelector("#roadAddressDetail").value;
+
+  let isValid = true;
+
+  if (zonecode === "") {
+    document.querySelector(".code").innerText = "우편번호를 올바르게 입력해주세요!";
+    isValid = false;
+  } else {
+    document.querySelector(".code").innerText = zonecode;
+  };
+
+  if (roadAddress === "") {
+    document.querySelector(".adr").innerText = "주소를 입력해주세요!";
+    isValid = false;
+  } else {
+    document.querySelector(".adr").innerText = roadAddress;
+  };
+
+  if (roadAddressDetail === "") {
+    document.querySelector(".adrDetail").innerText = "상세주소를 입력해주세요!";
+    isValid = false;
+  } else {
+    document.querySelector(".adrDetail").innerText = roadAddressDetail;
+  };
+});
+
+
+// name, phone error
+const namePhoneBtn = document.querySelector("#register-btn2");
+
+namePhoneBtn.addEventListener("click", (e) => {
+e.preventDefault();
+
+  const errorname = document.querySelector("#name").value;
+  const errorphone = document.querySelector("#num").value;
+
+  let isValid = true;
+
+  if (errorname === "") {
+    document.querySelector(".errorname").innerText = "성명을 올바르게 입력해주세요!";
+    isValid = false;
+  } else {
+    document.querySelector(".errorname").innerText = errorname;
+  };
+
+  if (errorphone === "") {
+    document.querySelector(".errornum").innerText = "휴대폰 번호를 올바르게 입력해주세요!";
+    isValid = false;
+  } else {
+    document.querySelector(".errornum").innerText = errorphone;
+  };
+});
+
+const finalSubmitBtn = document.querySelector("#finalSubmitBtn");
+finalSubmitBtn.addEventListener("click", () => {
+  location.href = "../final/final.html"
+})
+
+const accountShortcut = document.querySelector(".accountShortcut");
+accountShortcut.style.cursor = "pointer";
+accountShortcut.addEventListener("click", () => {
+  location.href = `../../my/my.html`;
+})
